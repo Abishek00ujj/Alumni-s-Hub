54b3a965f0a085fd4e53a371fb1f102a75f1bdb6
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-11-22 11:10:15 +0530

    loading added!

--- seniorscommunity/src/components/Signin.jsx
+++ seniorscommunity/src/components/Signin.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 import Stdimg from '../assets/clgstd.png'
+import { LoaderIcon } from 'lucide-react'
 export const Signin = () => {
+   const [Loading,setLoading]=useState(false);
+   const [Hide,setHide]=useState(true);
+   const HandleMonkey=()=>{
+      if(Hide)
+      {
+         setHide(false);
+      }
+      else{
+         setHide(true);
+      }
+   }
   return (
     <>
       <Navbar/>
       <div className='w-screen h-screen bg-black  justify-center items-center flex flex-row-reverse min-2xl:flex max-lg:flex-col p-5 max-2xl:justify-between p-5'>
-         <div className='w-[450px] h-[400px] bg-slate-50 rounded-xl flex flex-col space-y-5 justify-center items-center'>
-             <div className='text-4xl font-bold'>Sign in</div>
+         <div className='w-[400px] h-[380px] bg-slate-50 rounded-xl flex flex-col space-y-5 justify-center items-center'>
+            {
+               Hide?(
+                  <>
+                  <div className='text-4xl font-bold'>Sign in</div>
              <div> <span className='font-bold'>Students & Alumni</span> (SIET)</div>
              <div>Please select your year of passing to sign in.</div>
              <select name="" id="" className='p-3 w-[80%]'>
@@ -18,11 +33,19 @@ export const Signin = () => {
                 <option value="">2028</option>
              </select>
              <div>
-                <button className='bg-blue-950 text-white p-3 rounded-xl'>Sign in</button>
+                <button className='bg-blue-950 text-white p-3 rounded-xl' onClick={HandleMonkey}>Sign in</button>
              </div>
             <div className='text-orange-400'>
             <span className='font-bold text-black'>New to ALUMNIS-HUB?</span> <span className='underline'>Register</span>
+            
             </div>
+            </>
+               ):(
+                  <div>
+                     <LoaderIcon className='animate-spin' color='orange'/>
+                  </div>
+               )
+            }
          </div>
          <div className=' max-xl:w-[50%] max-sm:w-screen h-auto'>
               <img src={Stdimg} alt="" className='w-[500px] h-[400px]'/>
