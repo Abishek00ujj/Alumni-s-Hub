9b45aad6b0b4a01077bb736994ef215ea7eff240
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-11-22 14:48:11 +0530

    xx

--- seniorscommunity/src/components/Signin.jsx
+++ seniorscommunity/src/components/Signin.jsx
@@ -3,29 +3,51 @@ import Navbar from './Navbar'
 import Stdimg from '../assets/clgstd.png'
 import { LoaderIcon } from 'lucide-react'
 export const Signin = () => {
-   const [Loading,setLoading]=useState(false);
+   const [Loading,setLoading]=useState(true);
+   const [darkmode,setDarkmode]=useState(true);
    const [Hide,setHide]=useState(true);
-   const HandleMonkey=()=>{
-      if(Hide)
+   const [Mail,setMail]=useState(false);
+   const handleMail=()=>{
+      if(Mail)
       {
+         setMail(false);
+      }
+      else
+      {
+         setMail(true);
+      }
+   }
+   const HandleMonkey = () => {
+      setLoading(true);
+      const timeoutId = setTimeout(() => {
+         setLoading(false);
          setHide(false);
+      }, 1000);
+   
+      return () => clearTimeout(timeoutId); 
+   }
+   
+   const HandleDarkmode=()=>{
+      if(darkmode)
+      {
+         setDarkmode(false);
       }
       else{
-         setHide(true);
+         setDarkmode(true);
       }
    }
   return (
     <>
-      <Navbar/>
-      <div className='w-screen h-screen bg-black  justify-center items-center flex flex-row-reverse min-2xl:flex max-lg:flex-col p-5 max-2xl:justify-between p-5'>
-         <div className='w-[400px] h-[380px] bg-slate-50 rounded-xl flex flex-col space-y-5 justify-center items-center'>
+      <Navbar />
+      <div className={darkmode? ("w-screen h-screen bg-black  justify-center items-center flex flex-row-reverse min-2xl:flex max-lg:flex-col p-5 max-2xl:justify-between"):("w-screen h-screen bg-[#f8f8f8]  justify-center items-center flex flex-row-reverse min-2xl:flex max-lg:flex-col p-5 max-2xl:justify-between p-5")}>
+         <div className='w-[400px] h-[380px] bg-slate-50 rounded-xl flex flex-col space-y-5 justify-center items-center border border-black m-10'>
             {
                Hide?(
                   <>
-                  <div className='text-4xl font-bold'>Sign in</div>
+                  <div className='text-4xl font-bold' onClick={HandleDarkmode}>Sign in</div>
              <div> <span className='font-bold'>Students & Alumni</span> (SIET)</div>
              <div>Please select your year of passing to sign in.</div>
-             <select name="" id="" className='p-3 w-[80%]'>
+             <select name="" id="" className='p-3 w-[80%]' required>
                 <option value="">Please select your year of passing</option>
                 <option value="">2025</option>
                 <option value="">2026</option>
@@ -33,7 +55,7 @@ export const Signin = () => {
                 <option value="">2028</option>
              </select>
              <div>
-                <button className='bg-blue-950 text-white p-3 rounded-xl' onClick={HandleMonkey}>Sign in</button>
+                <button className='bg-blue-950 text-white p-3 rounded-xl' onClick={HandleMonkey}>Continue</button>
              </div>
             <div className='text-orange-400'>
             <span className='font-bold text-black'>New to ALUMNIS-HUB?</span> <span className='underline'>Register</span>
@@ -41,8 +63,27 @@ export const Signin = () => {
             </div>
             </>
                ):(
-                  <div>
-                     <LoaderIcon className='animate-spin' color='orange'/>
+                  <div className='w-[400px] h-[380px] flex flex-col m-10 items-center justify-center gap-5'>
+                     {
+                        Loading ?(
+                           <LoaderIcon className='animate-spin' color='orange'/> 
+                        ):(
+                           <>
+                           <div className='text-2xl font-bold font-sans'>
+                        Enter the registered email
+                     </div>
+                     <div>
+                        <input type="text" className='border border-black pl-8 pr-8 pt-3 pb-3 rounded-lg w-full'placeholder='Email' />
+                     </div>
+                     <div>
+                        <input type="password" name="" id="" className='border border-black pl-8 pr-8 pt-3 pb-3 rounded-lg w-full' placeholder='Password'/>
+                     </div>
+                     <div>
+                        <button className='bg-orange-400 p-3 rounded-lg'>SIGN IN</button>
+                     </div>
+                           </>
+                        )
+                     }
                   </div>
                )
             }
