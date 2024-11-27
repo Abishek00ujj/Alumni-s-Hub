e4aeb57987c111336e9e39f8d62416935e8dfe61
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-11-27 20:07:01 +0530

    m

--- seniorscommunity/src/components/Main.jsx
+++ seniorscommunity/src/components/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 import  Construction  from "../assets/construction.png"
-const Main = () => {
+const Main = (props) => {
   return (
     <>
+    <Navbar login={props.login}/>
     <div className='w-screen h-screen flex justify-center items-center'>
         <img src={Construction} alt="" className='w-[400px] h-[400px]'/>
     </div>
