ac18ae4f822545d292ae7f742749d5655e0f7b7b
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-12-29 20:47:32 +0530

    data fixed!

--- seniorscommunity/src/components/Profile.jsx
+++ seniorscommunity/src/components/Profile.jsx
@@ -1,154 +1,180 @@
-import React, { useEffect, useRef } from 'react'
-import Navbar from './Navbar';
-import { useState } from 'react'
-// import Navbar from './Navbar'
-import Stdimg from '../assets/clgstd.png'
-import { Link, useNavigate } from 'react-router-dom'
-import axios from 'axios';
-import { SetcompleteUser } from '../store';
-import { Loader2 } from 'lucide-react';
-const Profile = () => {
-    const navigate = useNavigate();
-    const [Loading, setLoading] = useState(false);
-    const [load,setLoad]=useState(true);
-    const [darkmode, setDarkmode] = useState(true);
-    const [Hide, setHide] = useState(true);
-    const [Mail, setMail] = useState(false);
-    const HandleMonkey = () => {
-        setHide(false);
-        setLoading(true);
-        setTimeout(() => setLoading(false), 1000);
-    }
-    const HandleDarkmode = () => {
-        if (darkmode) {
-            setDarkmode(false);
-        }
-        else {
-            setDarkmode(true);
-        }
-    }
-    let UserData = localStorage.getItem('data');
-    const linkedinRef = useRef(null);
-    const githubRef = useRef(null);
-    const leetcodeRef = useRef(null);
-    const HandleSubmit = async () => {
-        setLoad(true);
-        try {
-            const obj = {
-                id: UserData.Email,
-                Linkedin: linkedinRef.current.value,
-                Github: githubRef.current.value,
-                Leetcode: leetcodeRef.current.value
-            }
-            const res=await axios.post('https://alumni-s-hub.onrender.com/api/v1/addData',obj);
-            if (res.status === 200) {
-                localStorage.setItem('userData', JSON.stringify(obj));
-                console.log("UserData", JSON.parse(localStorage.getItem('userData')));
-                if (localStorage.getItem('data')) {
-                    console.log("Data", localStorage.getItem('data'));
-                } else {
-                    console.log("No data found in localStorage for 'data'");
-                }
-                if (res.status === 200)
-                    {
-                        navigate('/userprofile');
-                        localStorage.setItem('completeUser',res.data);
-                   }
-            }
-        }
-        catch (error) {
-            console.log(error);
-        }
-        finally{
-            setLoad(false);
-        }
-    }
-        useEffect(() => {
-            const fetchData = async () => {
-                try {
-                    const res = await axios.post('https://alumni-s-hub.onrender.com/api/v1/getData', { id: UserData.Email });
-                    console.log(res.status);
-                    if (res.status === 200)
-                    {
-                        navigate('/userprofile');
-                        localStorage.setItem('completeUser',res.data);
-                    }
-                } catch (error) {
-                    console.error(error);
-                }
-            };
-            fetchData();
-        }, []);
-    return (
-        <>
-            {
-                UserData ? (
-                    <>
-                        <Navbar />
-                        <div className={darkmode ? ("w-screen h-screen bg-black  justify-center items-center flex flex-row-reverse min-2xl:flex max-lg:flex-col p-5 max-2xl:justify-between") : ("w-screen h-screen bg-[#f8f8f8]  justify-center items-center flex flex-row-reverse min-2xl:flex max-lg:flex-col p-5 max-2xl:justify-between p-5")}>
-                            <div className='w-[400px] h-[380px] bg-slate-50 rounded-xl flex flex-col space-y-5 justify-center items-center border border-black m-10 p-5'>
-                                {
-                                    Hide ? (
-                                        <>
-                                            <div className='text-4xl font-bold' onClick={HandleDarkmode}>Profile creation🤩</div>
-                                            <div> <span className='font-bold'>Students & Alumni</span> (SIET)</div>
-                                            {/* <div>Please select your year of passing to sign in.</div> */}
-                                            <div>
-                                                <button className='bg-blue-950 text-white p-3 rounded-xl' onClick={HandleMonkey}>Create profile!</button>
-                                            </div>
-                                        </>
-                                    ) : (
-                                        <div className='w-full h-full flex flex-col justify-around items-center space-y-5'>
-                                            {
-                                                Loading ? (
-                                                    <div className='w-[400px] h-[380px] justify-center items-center flex'>
-                                                        <div className='w-[50px] h-[50px] bg-orange-400 rounded-full animate-spin'>
-                                                            <div className='w-[40px] h-[40px] bg-[#f8f8f8] rounded-full'></div>
-                                                        </div>
-                                                    </div>
-                                                ) : (
-                                                    <>
-                                                        <div className='text-2xl font-bold font-sans'>
-                                                            Your Profile details!🤩
-                                                        </div>
-                                                        <div>
-                                                            <input ref={linkedinRef} type="text" className='border border-black pl-8 pr-8 pt-3 pb-3 rounded-lg w-full' placeholder='Linkedin Profile Link*' />
-                                                        </div>
-                                                        <div>
-                                                            <input ref={leetcodeRef} type="text" className='border border-black pl-8 pr-8 pt-3 pb-3 rounded-lg w-full' placeholder='Leetcode username*' />
-                                                        </div>
-                                                        <div>
-                                                            <input ref={githubRef} type="text" name="" id="" className='border border-black pl-8 pr-8 pt-3 pb-3 rounded-lg w-full' placeholder='Github username*' />
-                                                        </div>
-                                                        <div>
-                                                            <button onClick={HandleSubmit} className='bg-blue-950 p-3 rounded-lg text-white'>{load?(
-                                                                <>
-                                                                 <div className='animate-spin w-full h-full text-white'>
-                                                                    <Loader2 size={50}/>
-                                                                 </div>
-                                                                </>
-                                                            ):("Show my profile🎉")}</button>
-                                                        </div>
-                                                    </>
-                                                )
-                                            }
-                                        </div>
-                                    )
-                                }
-                            </div>
-                            <div className=' max-xl:w-[50%] max-sm:w-screen h-auto'>
-                                <img src={Stdimg} alt="" className='w-[500px] h-[400px]' />
-                            </div>
-                        </div>
-                    </>
-                ) : (
-                    <>
-                        <Navbar />
-                    </>
-                )
-            }
-        </>
-    )
-}
+import React, { useEffect, useRef, useState } from "react";
+import Navbar from "./Navbar";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Loader2 } from "lucide-react";
+import Stdimg from "../assets/clgstd.png";
 
-export default Profile
+const Profile = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [load, setLoad] = useState(true);
+  const [darkmode, setDarkmode] = useState(true);
+  const [hide, setHide] = useState(true);
+
+  const linkedinRef = useRef(null);
+  const githubRef = useRef(null);
+  const leetcodeRef = useRef(null);
+
+  const userData = JSON.parse(localStorage.getItem("data"));
+
+  const handleMonkey = () => {
+    setHide(false);
+    setLoading(true);
+    setTimeout(() => setLoading(false), 1000);
+  };
+
+  const handleDarkmode = () => {
+    setDarkmode((prev) => !prev);
+  };
+
+  const handleSubmit = async () => {
+    setLoad(true);
+    try {
+      if (!userData?.Email) {
+        throw new Error("User data is missing!");
+      }
+
+      const obj = {
+        id: userData.Email,
+        Linkedin: linkedinRef.current.value,
+        Github: githubRef.current.value,
+        Leetcode: leetcodeRef.current.value,
+      };
+
+      const res = await axios.post(
+        "https://alumni-s-hub.onrender.com/api/v1/addData",
+        obj
+      );
+
+      if (res.status === 200) {
+        localStorage.setItem("userData", JSON.stringify(obj));
+        navigate("/userprofile");
+        localStorage.setItem("completeUser", JSON.stringify(res.data));
+      }
+    } catch (error) {
+      console.error("Error submitting profile data:", error.message);
+    } finally {
+      setLoad(false);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (!userData?.Email) {
+          throw new Error("User data is missing!");
+        }
+
+        const res = await axios.post(
+          "https://alumni-s-hub.onrender.com/api/v1/getData",
+          { id: userData.Email }
+        );
+
+        if (res.status === 200) {
+          localStorage.setItem("completeUser", JSON.stringify(res.data));
+          navigate("/userprofile");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {userData ? (
+        <>
+          <Navbar />
+          <div
+            className={`w-screen h-screen ${
+              darkmode ? "bg-black" : "bg-[#f8f8f8]"
+            } justify-center items-center flex flex-row-reverse min-2xl:flex max-lg:flex-col p-5 max-2xl:justify-between`}
+          >
+            <div className="w-[400px] h-[380px] bg-slate-50 rounded-xl flex flex-col space-y-5 justify-center items-center border border-black m-10 p-5">
+              {hide ? (
+                <>
+                  <div className="text-4xl font-bold" onClick={handleDarkmode}>
+                    Profile creation 🤩
+                  </div>
+                  <div>
+                    <span className="font-bold">Students & Alumni</span> (SIET)
+                  </div>
+                  <div>
+                    <button
+                      className="bg-blue-950 text-white p-3 rounded-xl"
+                      onClick={handleMonkey}
+                    >
+                      Create profile!
+                    </button>
+                  </div>
+                </>
+              ) : (
+                <div className="w-full h-full flex flex-col justify-around items-center space-y-5">
+                  {loading ? (
+                    <div className="w-[400px] h-[380px] justify-center items-center flex">
+                      <div className="w-[50px] h-[50px] bg-orange-400 rounded-full animate-spin">
+                        <div className="w-[40px] h-[40px] bg-[#f8f8f8] rounded-full"></div>
+                      </div>
+                    </div>
+                  ) : (
+                    <>
+                      <div className="text-2xl font-bold font-sans">
+                        Your Profile details! 🤩
+                      </div>
+                      <input
+                        ref={linkedinRef}
+                        type="text"
+                        className="border border-black pl-8 pr-8 pt-3 pb-3 rounded-lg w-full"
+                        placeholder="LinkedIn Profile Link*"
+                      />
+                      <input
+                        ref={leetcodeRef}
+                        type="text"
+                        className="border border-black pl-8 pr-8 pt-3 pb-3 rounded-lg w-full"
+                        placeholder="Leetcode username*"
+                      />
+                      <input
+                        ref={githubRef}
+                        type="text"
+                        className="border border-black pl-8 pr-8 pt-3 pb-3 rounded-lg w-full"
+                        placeholder="GitHub username*"
+                      />
+                      <button
+                        onClick={handleSubmit}
+                        className="bg-blue-950 p-3 rounded-lg text-white"
+                      >
+                        {load ? (
+                          <div className="animate-spin w-full h-full text-white">
+                            <Loader2 size={50} />
+                          </div>
+                        ) : (
+                          "Show my profile 🎉"
+                        )}
+                      </button>
+                    </>
+                  )}
+                </div>
+              )}
+            </div>
+            <div className="max-xl:w-[50%] max-sm:w-screen h-auto">
+              <img
+                src={Stdimg}
+                alt=""
+                className="w-[500px] h-[400px]"
+              />
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <Navbar />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Profile;
--- seniorscommunity/src/components/UserProfile.jsx
+++ seniorscommunity/src/components/UserProfile.jsx
@@ -2,29 +2,44 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import axios from "axios";
 import { Loader2 } from "lucide-react";
+
 const UserProfile = () => {
   const [userData, setUserData] = useState(null);
   const [gitData, setGitData] = useState(null);
 
   const fetchGitHubData = async (githubUsername) => {
     try {
-      const res = await axios.get(`https://api.github.com/users/${githubUsername}`);
+      const res = await axios.get(
+        `https://api.github.com/users/${githubUsername}`
+      );
       if (res.status === 200) {
         setGitData(res.data);
       }
     } catch (error) {
-      console.error("Error fetching GitHub data:", error);
+      console.error("Error fetching GitHub data:", error.message);
     }
   };
+
   useEffect(() => {
     try {
+      if(localStorage.getItem("completeUser"))
+      {
+        const storedData = JSON.parse(localStorage.getItem("completeUser"));
+        if (storedData) {
+          setUserData(storedData.data);
+          fetchGitHubData(storedData.data.Github);
+        }
+      }
+      else
+      {
       const storedData = JSON.parse(localStorage.getItem("userData"));
       if (storedData) {
         setUserData(storedData);
         fetchGitHubData(storedData.Github);
       }
+    }
     } catch (error) {
-      console.error("Error retrieving user data:", error);
+      console.error("Error retrieving user data:", error.message);
     }
   }, []);
 
@@ -34,42 +49,42 @@ const UserProfile = () => {
       <div className="w-screen bg-black flex flex-col space-y-5">
         {gitData && (
           <div className="w-full h-auto text-white flex flex-col justify-center items-center rounded-lg border border-white bg-[#121212] mt-5 mb-2">
-             <p className="w-full flex justify-center text-2xl max-2xl:justify-start">Username: {gitData.login}</p>
+            <p className="w-full flex justify-center text-2xl">
+              Username: {gitData.login}
+            </p>
             <img
               src={gitData.avatar_url}
               className="w-[200px] h-[200px] rounded-full"
               alt={`${gitData.login}'s avatar`}
             />
-            <p className="w-full flex justify-center text-2xl max-2xl:justify-start">{gitData.name}</p>
+            <p>{gitData.name}</p>
             <p>{gitData.bio}</p>
-            <p>Website: <a href={gitData.blog} target="_blank">{gitData.blog}</a></p>
+            <p>
+              Website:{" "}
+              <a href={gitData.blog} target="_blank" rel="noopener noreferrer">
+                {gitData.blog}
+              </a>
+            </p>
           </div>
         )}
-
         {userData ? (
           <div className="w-full h-full flex justify-center items-center flex-col space-y-5">
             <p className="text-2xl text-white">Total problems solved!</p>
             <img
-              src={`https://leetcard.jacoblin.cool/${userData.Leetcode}?theme=dark&font=Nunito&ext=heatmap`}
+              src={`https://leetcard.jacoblin.cool/${userData.Leetcode}?theme=dark`}
               alt="Leetcode stats"
             />
-            <h2 className="text-2xl text-white">Github stats 🎉</h2>
+            <h2 className="text-2xl text-white">GitHub stats 🎉</h2>
             <img
-              src={`https://streak-stats.demolab.com/?user=${userData.Github}&count_private=true&theme=react&border_radius=10`}
+              src={`https://streak-stats.demolab.com/?user=${userData.Github}&count_private=true&theme=react`}
               alt="GitHub streak stats"
-            />
-            <img
-              src={`https://github-readme-stats.vercel.app/api?username=${userData.Github}&show_icons=true&theme=react&rank_icon=github&border_radius=10`}
-              alt="GitHub readme stats"
-            />
-            <img
-              src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${userData.Github}&hide=HTML&langs_count=8&layout=compact&theme=react&border_radius=10&size_weight=0.5&count_weight=0.5&exclude_repo=github-readme-stats`}
-              alt="Top languages"
             />
           </div>
         ) : (
           <div className="w-screen h-screen flex justify-center items-center">
-              <p className="text-white text-center mt-5 animate-spin"><Loader2 color="white" size={50}/></p>
+            <p className="text-white text-center mt-5 animate-spin">
+              <Loader2 color="white" size={50} />
+            </p>
           </div>
         )}
       </div>
