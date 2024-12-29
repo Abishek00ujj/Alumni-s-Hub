4d052b1c186894a2a684f878536416f301b7f46e
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-12-29 21:08:05 +0530

    github added~!

--- seniorscommunity/src/components/UserProfile.jsx
+++ seniorscommunity/src/components/UserProfile.jsx
@@ -79,6 +79,8 @@ const UserProfile = () => {
               src={`https://streak-stats.demolab.com/?user=${userData.Github}&count_private=true&theme=react`}
               alt="GitHub streak stats"
             />
+            <img src={`https://github-readme-stats.vercel.app/api?username=${userData.Github}&show_icons=true&theme=react&rank_icon=github&border_radius=10" alt="readme stats`}/>
+            <img  align={`center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=${userData.Github}&hide=HTML&langs_count=8&layout=compact&theme=react&border_radius=10&size_weight=0.5&count_weight=0.5&exclude_repo=github-readme-stats" alt="top langs`} />
           </div>
         ) : (
           <div className="w-screen h-screen flex justify-center items-center">
